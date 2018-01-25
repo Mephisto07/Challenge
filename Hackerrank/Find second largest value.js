b78edd14abe86a2ad9726dc4f82b978b8c311418
Nicolas Segeti <nick_nds07@hotmail.com>
2018-01-25 11:08:44 -0200

    Update Find second largest value.js

--- Hackerrank/Find second largest value.js
+++ Hackerrank/Find second largest value.js
@@ -1,9 +1,9 @@
 //https://www.hackerrank.com/challenges/js10-arrays/problem
 function getSecondLargest(nums) {
     nums  = nums.sort(function(x, y) { return x < y; }); //sort desc
-    nums = nums.filter(function(item,pos) { return nums.indexOf(item) == pos; }); //unique values
-    
+    nums = nums.filter(function(item,pos) { return nums.indexOf(item) == pos; }); //unique values    
     return nums[1];
 }
 
 getSecondLargest([2,3,6,6,5]);
+//returns 5
